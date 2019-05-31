76c635897378d5b6c44dc301886352eb8736bc0b
Stanislav2014 <s_admin@mail.ru>
2019-05-31 14:32:11 +0300

    add test-task

---
+++ BD.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 31.05.19
+ * Time: 13:47
+ */
+
+
+class DbNode
+{
+    public function getDirs($id)
+    {
+        $pdo
+            return 
+    }
+
+};
